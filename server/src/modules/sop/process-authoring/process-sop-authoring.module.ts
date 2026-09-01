8f20be616f5c14403b0952733fb6527a0a8a1a33
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-01 18:25:21 +0700

    feat: wire Pelaksana snapshots into Process workbench

--- server/src/modules/sop/process-authoring/process-sop-authoring.module.ts
+++ server/src/modules/sop/process-authoring/process-sop-authoring.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ProcessModule } from '../../core/process/process.module';
 import { SopCatalogModule } from '../catalog/sop-catalog.module';
+import { PelaksanaModule } from '../pelaksana/pelaksana.module';
 import { ProcessBoundSopGuard } from './process-bound-sop.guard';
 import { ProcessSopAuthoringController } from './process-sop-authoring.controller';
 import { ProcessSopAuthoringService } from './process-sop-authoring.service';
 
 @Module({
-  imports: [ProcessModule, SopCatalogModule],
+  imports: [ProcessModule, SopCatalogModule, PelaksanaModule],
   controllers: [ProcessSopAuthoringController],
   providers: [
     ProcessSopAuthoringService,
