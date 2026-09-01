6af550d700a1fe03f10bb36ccc6c8ca11d7589ab
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-01 18:21:09 +0700

    refactor: wire Process context into procedure module

--- server/src/modules/sop/prosedur/sop-prosedur.module.ts
+++ server/src/modules/sop/prosedur/sop-prosedur.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../core/auth/auth.module';
 import { OpdModule } from '../../core/opd/opd.module';
+import { ProcessModule } from '../../core/process/process.module';
 import { SopCatalogModule } from '../catalog/sop-catalog.module';
 import { SopProsedurController } from './sop-prosedur.controller';
 import { SopProsedurRepository } from './sop-prosedur.repository';
 import { SopProsedurService } from './sop-prosedur.service';
 
 @Module({
-  imports: [AuthModule, OpdModule, SopCatalogModule],
+  imports: [AuthModule, OpdModule, ProcessModule, SopCatalogModule],
   controllers: [SopProsedurController],
   providers: [SopProsedurService, SopProsedurRepository],
 })
