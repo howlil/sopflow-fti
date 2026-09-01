7ed43991dab0be4178890ab1f729dd0360ba2e99
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-01 18:24:48 +0700

    feat: wire Pelaksana snapshots into procedure responses

--- server/src/modules/sop/prosedur/sop-prosedur.module.ts
+++ server/src/modules/sop/prosedur/sop-prosedur.module.ts
@@ -3,12 +3,13 @@ import { AuthModule } from '../../core/auth/auth.module';
 import { OpdModule } from '../../core/opd/opd.module';
 import { ProcessModule } from '../../core/process/process.module';
 import { SopCatalogModule } from '../catalog/sop-catalog.module';
+import { PelaksanaModule } from '../pelaksana/pelaksana.module';
 import { SopProsedurController } from './sop-prosedur.controller';
 import { SopProsedurRepository } from './sop-prosedur.repository';
 import { SopProsedurService } from './sop-prosedur.service';
 
 @Module({
-  imports: [AuthModule, OpdModule, ProcessModule, SopCatalogModule],
+  imports: [AuthModule, OpdModule, ProcessModule, SopCatalogModule, PelaksanaModule],
   controllers: [SopProsedurController],
   providers: [SopProsedurService, SopProsedurRepository],
 })
