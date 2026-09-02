1769fcd6f3396949ee3f43e80ae6f250334f43eb
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-03 01:59:40 +0700

    feat(m7): add target platform account DTO

---
+++ server/src/modules/core/pengguna/dto/create-platform-account.dto.ts
@@ -0,0 +1,4 @@
+import { CreatePenggunaProfilDto } from './create-pengguna-profil.dto';
+
+/** Target FTI account provisioning intentionally hides legacy OPD/workflow-role fields. */
+export class CreatePlatformAccountDto extends CreatePenggunaProfilDto {}
