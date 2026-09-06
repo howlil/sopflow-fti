3879927ca44ace5852004fae98f322ab82ca63f3
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-06 07:18:01 +0700

    feat(process): wire owner self-service services

--- server/src/modules/core/process/process.module.ts
+++ server/src/modules/core/process/process.module.ts
@@ -4,22 +4,38 @@ import { OrganizationalAuthorityService } from './organizational-authority.servi
 import { ProcessContextController } from './process-context.controller';
 import { ProcessContextService } from './process-context.service';
 import { ProcessController } from './process.controller';
+import { ProcessInvitationController } from './process-invitation.controller';
+import { ProcessOwnerAuthorityController } from './process-owner-authority.controller';
+import { ProcessOwnerAuthorityService } from './process-owner-authority.service';
+import { ProcessOwnerController } from './process-owner.controller';
+import { ProcessOwnerService } from './process-owner.service';
 import { ProcessRepository } from './process.repository';
 import { ProcessService } from './process.service';
 
 @Module({
-  controllers: [ProcessController, ProcessContextController, OrganizationalAuthorityController],
+  controllers: [
+    ProcessController,
+    ProcessContextController,
+    OrganizationalAuthorityController,
+    ProcessOwnerAuthorityController,
+    ProcessOwnerController,
+    ProcessInvitationController,
+  ],
   providers: [
     ProcessRepository,
     ProcessService,
     ProcessContextService,
     OrganizationalAuthorityService,
+    ProcessOwnerAuthorityService,
+    ProcessOwnerService,
   ],
   exports: [
     ProcessRepository,
     ProcessService,
     ProcessContextService,
     OrganizationalAuthorityService,
+    ProcessOwnerAuthorityService,
+    ProcessOwnerService,
   ],
 })
 export class ProcessModule {}
