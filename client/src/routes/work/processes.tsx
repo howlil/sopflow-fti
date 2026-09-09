48d6ffd558b18f63a0fdf0ae24296bc569684fd3
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-10 06:09:05 +0700

    guard process management by Penanggung Jawab capability

--- client/src/routes/work/processes.tsx
+++ client/src/routes/work/processes.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { requirePenanggungJawabProsesBisnis } from '@/lib/auth/require-work-capability'
 import { ProcessManagementPage } from '@/pages/work/ProcessManagementPage'
 
 export const Route = createFileRoute('/work/processes')({
+  beforeLoad: requirePenanggungJawabProsesBisnis(),
   component: ProcessManagementPage,
 })
