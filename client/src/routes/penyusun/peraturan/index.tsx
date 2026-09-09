2156a3aa3bcb438b651b03a091ebdfe888bddaa3
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-10 06:09:27 +0700

    guard Peraturan catalog by process responsibility

--- client/src/routes/penyusun/peraturan/index.tsx
+++ client/src/routes/penyusun/peraturan/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { requireProsesBisnisResponsibility } from '@/lib/auth/require-work-capability'
 import { ManajemenPeraturan } from '@/pages/penyusun/peraturan/ManajemenPeraturan'
 
 export const Route = createFileRoute('/penyusun/peraturan/')({
+  beforeLoad: requireProsesBisnisResponsibility(),
   component: ManajemenPeraturan,
 })
