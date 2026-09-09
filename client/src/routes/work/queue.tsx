122d76f3b74a125145f6f87b95ba3381761f001a
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-10 06:09:11 +0700

    guard SOP work queue by active process responsibility

--- client/src/routes/work/queue.tsx
+++ client/src/routes/work/queue.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { requireProsesBisnisResponsibility } from '@/lib/auth/require-work-capability'
 import { HalamanAntrianKerjaProsesBisnis } from '@/pages/work/HalamanAntrianKerjaProsesBisnis'
 
 export const Route = createFileRoute('/work/queue')({
+  beforeLoad: requireProsesBisnisResponsibility(),
   component: HalamanAntrianKerjaProsesBisnis,
 })
