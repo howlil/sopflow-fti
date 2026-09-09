e2d5abf9547560e68394710c79a24dc4a431b0ea
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-10 06:07:53 +0700

    guard global Pelaksana mutations by process responsibility

--- server/src/modules/sop/pelaksana/pelaksana.module.ts
+++ server/src/modules/sop/pelaksana/pelaksana.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../core/auth/auth.module';
+import { ProsesBisnisModule } from '../../core/proses-bisnis/proses-bisnis.module';
 import { PelaksanaController } from './pelaksana.controller';
 import { PelaksanaRepository } from './pelaksana.repository';
 import { PelaksanaService } from './pelaksana.service';
 import { PelaksanaSnapshotService } from './pelaksana-snapshot.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ProsesBisnisModule],
   controllers: [PelaksanaController],
   providers: [PelaksanaService, PelaksanaRepository, PelaksanaSnapshotService],
   exports: [PelaksanaSnapshotService],
