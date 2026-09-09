f0db7d738298071029f5afda208f7080a652e9f5
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-10 06:09:33 +0700

    guard Pelaksana catalog by process responsibility

--- client/src/routes/penyusun/pelaksana/index.tsx
+++ client/src/routes/penyusun/pelaksana/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { requireProsesBisnisResponsibility } from '@/lib/auth/require-work-capability'
 import { PelaksanaSOP } from '@/pages/penyusun/pelaksana/PelaksanaSOP'
 
 export const Route = createFileRoute('/penyusun/pelaksana/')({
+  beforeLoad: requireProsesBisnisResponsibility(),
   component: PelaksanaSOP,
 })
